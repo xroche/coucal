30104332b1a4053f796ca4a92df687a7426b5d01
Xavier Roche <roche@httrack.com>
2014-06-15 10:29:22 +0200

    Added coucal_fetch_value_hashes()

--- sample.c
+++ sample.c
@@ -30,6 +30,10 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+ * Sample: String => String hashtable.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
